22b3ecac55ba8859a79cd13ddbd0bfc873dc673b
CuteVanyaaa <Finber1982@gmail.com>
2025-05-23 22:04:32 +0700

    zadanie 3

--- Laba 1/Задание 2.py
+++ Laba 1/Задание 2.py
@@ -1,1 +1,26 @@
-Задание 1.pyЗадание 1.py
+def combinationSum2(candidates, target):
+  result = []
+  candidates.sort()
+
+  def backtrack(combination, remaining, start):
+    if remaining == 0:
+      result.append(combination.copy())
+      return
+    if remaining < 0:
+      return
+
+    for i in range(start, len(candidates)):
+      if i > start and candidates[i] == candidates[i - 1]:
+        continue
+
+      combination.append(candidates[i])
+      backtrack(combination, remaining - candidates[i], i + 1)
+      combination.pop()
+
+  backtrack([], target, 0)
+  return result
+
+candidates1 = [2, 5, 2, 1, 2]
+target1 = 5
+result1 = combinationSum2(candidates1, target1)
+print(result1)
