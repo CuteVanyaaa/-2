a52798d3b21fc85c6934eb14d11710e10f796a9d
CuteVanyaaa <Finber1982@gmail.com>
2025-05-23 22:02:31 +0700

    zadanie 1

---
+++ Laba 1/Задание 1.py
@@ -0,0 +1,11 @@
+def count_jewels(J, S):
+  jewels = set(J)
+  count = 0
+  for stone in S:
+    if stone in jewels:
+      count += 1
+  return count
+J = "ab"
+S = "aabbccd"
+result = count_jewels(J, S)
+print(result)
