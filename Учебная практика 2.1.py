83cb9a39b3190ede8f4596072f8bffcae4c05ad7
CuteVanyaaa <Finber1982@gmail.com>
2025-05-23 21:41:26 +0700

    zadanie 2

--- Учебная практика 2.1.py
+++ Учебная практика 2.1.py
@@ -1,12 +1,26 @@
-def count_jewels(J, S):
-  jewels = set(J)
-  count = 0
-  for stone in S:
-    if stone in jewels:
-      count += 1
-  return count
+def combinationSum2(candidates, target):
+  result = []
+  candidates.sort()
 
-J = "ab"
-S = "aabbccd"
-result = count_jewels(J, S)
-print(result)
+  def backtrack(combination, remaining, start):
+    if remaining == 0:
+      result.append(combination.copy())
+      return
+    if remaining < 0:
+      return
+
+    for i in range(start, len(candidates)):
+      if i > start and candidates[i] == candidates[i - 1]:
+        continue
+
+      combination.append(candidates[i])
+      backtrack(combination, remaining - candidates[i], i + 1)
+      combination.pop()
+
+  backtrack([], target, 0)
+  return result
+
+candidates1 = [2, 5, 2, 1, 2]
+target1 = 5
+result1 = combinationSum2(candidates1, target1)
+print(result1)
